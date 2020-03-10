ba6181704e75b7fb8a8824b18087f7d2d28418db
saguado-apsl <saguado@apsl.net>
2020-03-10 17:21:48 +0100

    list

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from blog.models import Post
+
 
 # Create your views here.
 
 def post_list(request):
 
-    return render(request, 'post_list.html', {})
+    list = Post.objects.all()
+    print(list)
+    return render(request, 'post_list.html', {'list':list})
 
 
 
